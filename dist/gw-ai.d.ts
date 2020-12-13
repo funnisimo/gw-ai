55b1effc2ce2a6cbd0cdca61fef741b62d875d16
Funnisimo <funnisimo@gmail.com>
2020-12-13 14:39:18 -0600

    created a single .d.ts file for types

---
+++ dist/gw-ai.d.ts
@@ -0,0 +1,18 @@
+declare type AIFunction = (actor: Actor) => Promise<boolean>;
+interface Config {
+    fns: Array<AIFunction>;
+}
+interface Actor {
+    ai: Config;
+}
+interface Actions {
+    endTurn: (actor: Actor, cost?: number) => void;
+}
+declare function configure(actions: Partial<Actions>): void;
+declare function addFunction(id: string, fn: AIFunction): boolean;
+
+declare function addKind(id: string, config: Config): boolean;
+declare function make(opts?: any): Config;
+declare function execute(actor: Actor): Promise<boolean>;
+
+export { addFunction, addKind, configure, execute, make };
--- rollup.config.js
+++ rollup.config.js
@@ -2,8 +2,14 @@
 // GW-AI: rollup.config.js
 
 import { terser } from "rollup-plugin-terser";
+import dts from "rollup-plugin-dts";
 
 export default [
+  {
+    input: "./js/types/index.d.ts",
+    output: [{ file: "dist/gw-ai.d.ts", format: "es" }],
+    plugins: [dts()],
+  },
 {
   input: 'js/gw.js',
   output: {
